2d3e2b34d82d81c2fa77310c1ecef53535e42459
Mateusz Sulimowicz <mateusz.sulimowicz@syncron.com>
2022-03-25 10:10:38 +0100

    Add AnswersController to app

--- src/server.js
+++ src/server.js
@@ -1,21 +1,26 @@
 const App = require("./App");
 const CreateFormController = require("./controllers/CreateFormController");
-const InMemoryFormRepo = require ("../test/InMemoryRepo");
+const InMemoryRepo = require ("../test/InMemoryRepo");
 const dotenv = require("dotenv");
+const SubmitAnswersController = require("./controllers/SubmitAnswersController");
 
 // To jest plik główny projektu.
 
 // Wczytujemy zmienne z pliku .env.
-const config = dotenv.config;
+const config = dotenv.config();
 if (config.error) {
   console.log(config.error);
 }
 
 // TODO: zamienić na mongo
-let repo = new InMemoryFormRepo();
+let formRepo = new InMemoryRepo();
+let answerRepo = new InMemoryRepo();
 
 // Tutaj należy dodawać nowe kontrolery.
-const controllers = [new CreateFormController(repo)];
+const controllers = [
+  new CreateFormController(formRepo),
+  new SubmitAnswersController(formRepo, answerRepo)
+];
 
 const app = new App(controllers);
 
