139b4d869f4d8dedcc5aea8b83fe749f4a158cbb
Mateusz Sulimowicz <mateusz.sulimowicz@syncron.com>
2022-03-25 10:54:03 +0100

    Fix GetResultsController

--- src/controllers/GetResultsController.js
+++ src/controllers/GetResultsController.js
@@ -18,17 +18,13 @@ module.exports = class GetResultsController {
       .get(this.get);
   }
 
-<<<<<<< HEAD
-  post = async (req, res) => {
-=======
   get = async (req, res) => {
     const formId = req.body.id;
 
     if (this.repo.contains(formId)) {
       res.send(this.repo.get(formId));
     } else {
-<<<<<<< HEAD
-      res.status(404).send(`Form ${formId} does not exist.`);
+      res.status(404).send(`Results for form ${formId} do not exist.`);
     }
   }
 
