b75249648d243ba34164c464074e7508f32f8c85
Mateusz Malinowski <malina314@op.pl>
2022-05-05 15:18:09 +0200

    Add connecting to database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const CreateFormController = require("./controllers/FormController");
 const dotenv = require("dotenv");
 const SubmitAnswersController = require("./controllers/AnswersController");
 const GetResultsController = require("./controllers/ResultsController");
+const Database = require("./database/Database");
 
 // To jest plik główny projektu.
 
@@ -11,6 +12,8 @@ const config = dotenv.config();
 if (config.error) {
   console.log(config.error);
 }
+
+Database.connect();
 
 // Tutaj należy dodawać nowe kontrolery.
 const controllers = [
